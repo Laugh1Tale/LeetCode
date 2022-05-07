d81bbc061f3855988cd009362fba276ca63ca9af
Laugh1Tale <saharus@outlook.com>
2022-05-07 15:48:23 +0500

    Solved Number of Steps to Reduce a Number to Zero.

---
+++ LeetCode/Easy/NumberOfStepsToReduceANumberToZero.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy
+{
+    public class NumberOfStepsToReduceANumberToZero
+    {
+        private int count = 0;
+
+        public int NumberOfSteps(int num)
+        {
+            if (num == 0)
+                return count;
+            count++;
+            if (num % 2 == 0)
+                return NumberOfSteps(num / 2);
+            else
+                return NumberOfSteps(num - 1);
+        }
+    }
+}
