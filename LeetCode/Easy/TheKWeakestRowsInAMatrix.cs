e844ba1151a992179e52b38a9a4e5e008cf1ebdd
Laugh1Tale <saharus@outlook.com>
2022-05-07 15:28:44 +0500

    Solved The K Weakest Rows in a Matrix.

---
+++ LeetCode/Easy/TheKWeakestRowsInAMatrix.cs
@@ -0,0 +1,15 @@
+﻿//Runtime: 167 ms, faster than 78.49% of C# online submissions for The K Weakest Rows in a Matrix.
+//Memory Usage: 44.7 MB, less than 80.21% of C# online submissions for The K Weakest Rows in a Matrix.
+
+namespace LeetCode.Easy
+{
+    public class TheKWeakestRowsInAMatrix
+    {
+        public int[] KWeakestRows(int[][] mat, int k) =>
+            mat.Select((value, index) => new { value, index })
+               .OrderBy(x => x.value.Sum())
+               .Take(k)
+               .Select(x => x.index)
+               .ToArray();
+    }
+}
