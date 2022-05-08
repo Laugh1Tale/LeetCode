c21954a509a52f0e3b0299c94fcbdd7fdc61837d
Laugh1Tale <saharus@outlook.com>
2022-05-08 18:43:51 +0500

    Solved Binary Search.

--- LeetCode/Easy/BinarySearch.cs
+++ LeetCode/Easy/BinarySearch.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Runtime: 154 ms, faster than 54.14% of C# online submissions for Binary Search.
+//Memory Usage: 41.5 MB, less than 74.84% of C# online submissions for Binary Search
 
 namespace LeetCode.Easy
 {
-    internal class BinarySearch
+    public class BinarySearch
     {
+        public int Search(int[] nums, int target)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            while (left != right)
+            {
+                if (nums[(right + left) / 2] < target)
+                    left = (right + left) / 2 + 1;
+                else
+                    right = (right + left) / 2;
+            }
+            if (nums[left] == target)
+                return left;
+            return -1;
+        }
     }
 }
