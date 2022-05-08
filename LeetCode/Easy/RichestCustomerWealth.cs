f3b974fc95e2882687f0ef25b891bcce05aa822b
Laugh1Tale <saharus@outlook.com>
2022-05-08 18:44:39 +0500

    Solved Richest Customer Wealth.

--- LeetCode/Easy/RichestCustomerWealth.cs
+++ LeetCode/Easy/RichestCustomerWealth.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Runtime: 87 ms, faster than 86.72% of C# online submissions for Richest Customer Wealth.
+//Memory Usage: 37.6 MB, less than 72.11% of C# online submissions for Richest Customer Wealth.
 
 namespace LeetCode.Easy
 {
-    internal class RichestCustomerWealth
+    public class RichestCustomerWealth
     {
+        public int MaximumWealth(int[][] accounts) =>
+            accounts.Select(x => x.Sum()).Max();
     }
 }
