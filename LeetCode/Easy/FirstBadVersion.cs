e8dec3d9f05d0d47f4bfcd8efc2bb8b082e56843
Laugh1Tale <saharus@outlook.com>
2022-05-28 19:52:15 +0500

    Solved First Bad Version

---
+++ LeetCode/Easy/FirstBadVersion.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Easy
+{
+    public class FirstBadVersion
+    {
+        public int FindFirstBadVersion(int n)
+        {
+            var left = 0;
+            var right = n;
+            while (left != right)
+            {
+                Console.WriteLine(left + "   " + right);
+                if (!IsBadVersion((right + left) / 2))
+                    left = (right + left) / 2 + 1;
+                else
+                    right = (right + left) / 2;
+            }
+            if (IsBadVersion(left))
+                return left;
+            Console.WriteLine(left + "   " + right);
+            return -1;
+        }
+
+
+        private bool IsBadVersion(int n)
+        {
+            return n >= 1;
+        }
+    }
+}
