54ca5ae1c0e6d07b2952e4160d87579e28f8c217
Laugh1Tale <saharus@outlook.com>
2022-05-28 20:23:00 +0500

    Solved Search Insert Position

---
+++ LeetCode/Easy/SearchInsertPosition.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    internal class SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            if (nums[nums.Length - 1] < target)
+                return nums.Length;
+            var left = 0;
+            var right = nums.Length - 1;
+            while (left != right)
+            {
+                if (nums[(right + left) / 2] < target)
+                    left = (right + left) / 2 + 1;
+                else
+                    right = (right + left) / 2;
+            }
+            return left;
+        }
+    }
+}
