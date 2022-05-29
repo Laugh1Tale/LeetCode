e167428335d1e577d30b1f84bd133622d3d44bba
Laugh1Tale <saharus@outlook.com>
2022-05-29 19:11:36 +0500

    Solved Squares of a Sorted Array

---
+++ LeetCode/Easy/SquaresOfASortedArray.cs
@@ -0,0 +1,11 @@
+﻿//Runtime: 221 ms, faster than 54.35% of C# online submissions for Squares of a Sorted Array.
+//Memory Usage: 49.1 MB, less than 36.55% of C# online submissions for Squares of a Sorted Array.
+
+namespace LeetCode.Easy
+{
+    public class SquaresOfASortedArray
+    {
+        public int[] MakeSquaresOfASortedArray(int[] nums) =>
+            nums.Select(x => x * x).OrderBy(x => x).ToArray();
+    }
+}
