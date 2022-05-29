2d8945e24336bf756cfb978e00ad85643a417e8a
Laugh1Tale <saharus@outlook.com>
2022-05-29 19:11:09 +0500

    Solved Rotate Array

---
+++ LeetCode/Easy/RotateArray.cs
@@ -0,0 +1,17 @@
+﻿//Runtime: 217 ms, faster than 55.11% of C# online submissions for Rotate Array.
+//Memory Usage: 49.1 MB, less than 82.08% of C# online submissions for Rotate Array.
+
+namespace LeetCode.Easy
+{
+    public class RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k %= nums.Length;
+            nums.Skip(nums.Length - k).Take(k)
+                .Concat(nums.Take(nums.Length - k))
+                .ToArray()
+                .CopyTo(nums, 0);
+        }
+    }
+}
