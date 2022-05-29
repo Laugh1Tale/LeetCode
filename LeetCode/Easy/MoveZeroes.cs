65fa066b6a358dea0f5bc3d7cb0f6dd10ba20666
Laugh1Tale <saharus@outlook.com>
2022-05-29 19:50:56 +0500

    Solved Move Zeroes

---
+++ LeetCode/Easy/MoveZeroes.cs
@@ -0,0 +1,26 @@
+﻿//Runtime: 218 ms, faster than 52.42% of C# online submissions for Move Zeroes.
+//Memory Usage: 46.9 MB, less than 86.88% of C# online submissions for Move Zeroes.
+
+namespace LeetCode.Easy
+{
+    public class MoveZeroes
+    {
+        public void MoveZeroesToEnd(int[] nums)
+        {
+            var indexes = nums.Select((value, index) => new { value, index })
+                .Where(x => x.value == 0)
+                .Select(x => x.index);
+            var listOfNums = nums.ToList();
+            var count = 0;
+            foreach (var index in indexes)
+            {
+                listOfNums.Add(nums[index]);
+                listOfNums.RemoveAt(index - count);
+                count++;
+            }
+            listOfNums
+                .ToArray()
+                .CopyTo(nums, 0);
+        }
+    }
+}
